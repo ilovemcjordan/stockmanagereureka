f3d6f0ae5ae402cf190ab1b842539e1e5c874834
Jian Xiao <dlxjian@cn.ibm.com>
2019-12-18 17:23:08 +0900

    test

---
+++ stockmanagereureka/excute.cmd
@@ -0,0 +1,2 @@
+cd target
+java -jar stockmanager-eureka-0.0.1-SNAPSHOT.jar > log2.txt
---
+++ stockmanagereureka/src/test/java/com/iiht/fsd/sba/StockMarketChartEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iiht.fsd.sba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockMarketChartEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
